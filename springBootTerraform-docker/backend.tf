299c510bb623e8c20daf0a7a9429a7889ff8a8bf
fardeen.mazumdar <fardeenkhalid@gmail.com>
2025-09-03 16:08:07 -0400

    Dockerized pet clinic

--- springBootTerraform-docker/backend.tf
+++ springBootTerraform-docker/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket         = "assignmentbucket20250901"
-    key            = "springBootTerraform-docker/dev/terraform.tfstate"
+    key            = "springBootTerraform/dev/terraform.tfstate"
     region         = "us-east-1"
     dynamodb_table = "test-iac"
     encrypt        = true
